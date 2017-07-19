5625458ebf6768d037bc165bf49215d79d373df9
viveksnh <sinhava@gmail.com>
2017-07-19 15:54:29 -0700

    Update script_switch_failure.R

--- script_switch_failure.R
+++ script_switch_failure.R
@@ -1,10 +1,13 @@
 # Reading in the data from european format CSV file
 library(data.table);library(sqldf)
 ##this works well
-#max number of fields in the dataaset - num of columns vary for each row
+#Reading sensor data
+#Data was in very bad format. The column headers were incomplete in the first row of csv
+#To address this issue, I am using a predefined list of col.names
+
+#max number of fields (columns) in the dataaset - num of columns vary for each row
 max_cols = max(count.fields("C:/Users/vsinha/Documents/SWITCH_EVENT_SENSORDATA_LIST.csv", sep=";"))
 
-#Reading sensor data
 dt_sensor = read.table("C:/Users/vsinha/Documents/SWITCH_EVENT_SENSORDATA_LIST.csv", header = FALSE, sep = ";", 
                        col.names = paste0("V",seq_len(max_cols)), fill = TRUE, dec=",", skip = 1,
                        colClasses = c("integer", "character", "numeric", "character",rep("numeric",720)))
@@ -28,13 +31,14 @@ dt = merge(dt_event, dt_sensor, by="event_id")
 #Creating a date field
 dt[, event_dt:=as.Date(event_time),]
 
-##Adding count uses before failure
+##Adding cumulative count of failures (ranked by date)
 dt[, n_failure := cumsum(event_result=="failure"), by=c("switch_id", "sensor_id")]
 #Adding a count of uses - resets at every failure
 dt[, n_uses:=cumsum(event_result!="failure"), by=c("switch_id", "sensor_id", "n_failure")]
 
 #Creating aggregated dataset using sqldf
 ##Aggregating dataset - example for creating 7 day average with 7 day blackout period
+#----!!! This is running out of memory. so moving top AZURE VM at this point.
 dt_temp = 
 result = sqldf(
 "Select dt.event_id, 
